875ce7e88d9d873819a67fcf8f94371822a511a7
unknown <e.thompson@erasolutions.us>
2024-08-10 11:31:44 -0400

    added the resources component includding the styling based off of the figma wireframe. like the other component made a json object then mapped through it for the component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Mission from '../components/Mission/Mission';
+import Resources from '../components/Resources/Resources';
 
 const HomePage = () => {
   return (
     <>
       <Header/>
       <Mission/>
+      <Resources/>
     </>
   );
 }
