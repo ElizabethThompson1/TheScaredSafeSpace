3648d1f6b4ca9eee02183f7962b44a320dce45f4
unknown <e.thompson@erasolutions.us>
2024-08-14 15:38:30 -0400

    aboutpage  section mission

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { AboutHeader } from '../components/About/Header';
+import { AboutHeader } from '../components/About/Header/Header';
+import AboutMission from '../components/About/AboutMission/AboutMission';
 
 const AboutPage = () => {
     return (
         <>
         <AboutHeader/>
+        <AboutMission/>
         </>
     )
 }
